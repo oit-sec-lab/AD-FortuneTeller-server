eb1cff7d17a0ad0b0aff0175e677b8ba85c3afb4
quantumshiro <hiro.1nakanishi@gmail.com>
2022-09-25 15:35:10 +0900

    make ad dict

--- src/checkURL.py
+++ src/checkURL.py
@@ -15,8 +15,30 @@ def predict(data):
     y_pred = np.where(y_pred > 0.5, 1, 0)
     return y_pred
 
-def main():
-    print(createCSV.results)
+def Getdata():
+    result = createCSV.results
+    data = []
+    for id in result:
+        data.append([result[id]["domain"], result[id]["network"], result[id]["extend"], result[id]["regex1"], result[id]["regex2"], result[id]["regex3"], result[id]["regex4"], result[id]["regex5"], result[id]["virustotal"]])
     
+    return data
+
+def makedict():
+    data = Getdata()
+    result = dict()
+    for i in range(len(data)):
+        """
+        {
+            1: {"ad": true or false},
+            2: {"ad": true or false}
+        }
+        """
+        id = i + 1
+        if predict([data[i]]) == 1:
+            result[id] = {"ad": True}
+        elif predict([data[i]]) == 0:
+            result[id] = {"ad": False}
+    print(result)
+       
 if __name__ == '__main__':
-    main()
+    makedict()
